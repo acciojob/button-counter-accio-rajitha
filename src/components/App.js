d64b81436cb768883ad7c1c8f672f8efeb4846e1
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-05-08 06:09:30 +0530

    app

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,18 @@
 import React from "react";
 import './../styles/App.css';
 
+
 const App = () => {
+
+  const [num, setNum] = React.useState(0); 
+  
   return (
     <div>
         {/* Do not remove the main div */}
+      <p>Button clicked {num} times</p>
+      <button  onClick={()=>{
+       setNum(num + 1)
+      }}>Click me</button>
     </div>
   )
 }
